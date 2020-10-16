3a8257566f261b397a4547c6f0297339f8552f8a
Andrew Coleburn <arcoleburn@gmail.com>
2020-10-16 16:06:34 -0400

    added header component

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 
 import RMDBLogo from '../../images/react-movie-logo.svg'
-import TMBDLogo from '../../images/tmdb_logo.svg'
+import TMDBLogo from '../../images/tmdb_logo.svg'
 
-import {Wrapper, Content, LogoImg, TMBDLogoImg} from ''./Headers.styles'
+import {Wrapper, Content, LogoImg, TMDBLogoImg} from './Header.styles'
+
+const Header = () => (
+    <Wrapper>
+        <Content>
+            <LogoImg src={RMDBLogo} alt='rmdb-logo'/>
+            <TMDBLogoImg src={TMDBLogo} alt='tmdb-logo'/>
+
+        </Content>
+    </Wrapper>
+)
+
+export default Header
