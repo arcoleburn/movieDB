0b656dd86fdbd1724399cbbc1caaee255fb9a16b
Andrew Coleburn <arcoleburn@gmail.com>
2020-10-16 11:50:57 -0400

    started on header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import RMDBLogo from '../../images/react-movie-logo.svg'
+import TMBDLogo from '../../images/tmdb_logo.svg'
+
+import {Wrapper, Content, LogoImg, TMBDLogoImg} from ''./Headers.styles'
