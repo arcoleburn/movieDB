34e6eeec7f7947ba8b6f632d9deb73b1246ebea4
Andrew Coleburn <arcoleburn@gmail.com>
2020-10-17 17:31:02 -0400

    added breadcrumb component, and main movie info component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from "react";
+//router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+///styles
 import { GlobalStyle } from "./GlobalStyle";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+import Movie from "./Components/Movie";
+import NotFound from "./Components/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Home />
-      <GlobalStyle />
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/:movieId">
+        <Movie />
+      </Route>
+      <Route  path="/*">
+        <NotFound />
+      </Route>
+    </Switch>
+    {/* <Home /> */}
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
---
+++ src/Components/Movie.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+//config
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
+//components
+import Grid from "./Grid";
+import Spinner from "./Spinner";
+import BreadCrumb from "./BreadCrumb";
+import MovieInfo from "./MovieInfo"
+//Hook
+import { useMovieFetch } from "../hooks/useMovieFetch";
+//image
+import NoImage from "../images/no_image.jpg";
+
+const Movie = () => {
+  const { movieId } = useParams();
+
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+
+  if (loading) return <Spinner />;
+  if (error) return <div> Something went Wrong...</div>;
+  return (
+    <>
+      <BreadCrumb movieTitle={movie.original_title} />
+      <MovieInfo movie={movie} />
+    </>
+  );
+};
+
+export default Movie;
